714de484c43223d00f9f7397394aab81ea04ef6a
devuri <devuriko@gmail.com>
2020-11-11 09:44:27 -0500

    composer update

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -6,4 +6,8 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'MembershipLock\\Admin\\MembershipAdmin' => $baseDir . '/src/Admin/MembershipAdmin.php',
+    'MembershipLock\\LockItdown' => $baseDir . '/src/LockItdown.php',
+    'MembershipLock\\WPAdminPage\\AdminPage' => $baseDir . '/src/WPAdminPage/AdminPage.php',
+    'MembershipLock\\WPAdminPage\\FormHelper' => $baseDir . '/src/WPAdminPage/FormHelper.php',
 );
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -20,11 +20,19 @@ class ComposerStaticInit78b2fc3ed8d44f0dd86ca5d881dc3ce2
         ),
     );
 
+    public static $classMap = array (
+        'MembershipLock\\Admin\\MembershipAdmin' => __DIR__ . '/../..' . '/src/Admin/MembershipAdmin.php',
+        'MembershipLock\\LockItdown' => __DIR__ . '/../..' . '/src/LockItdown.php',
+        'MembershipLock\\WPAdminPage\\AdminPage' => __DIR__ . '/../..' . '/src/WPAdminPage/AdminPage.php',
+        'MembershipLock\\WPAdminPage\\FormHelper' => __DIR__ . '/../..' . '/src/WPAdminPage/FormHelper.php',
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit78b2fc3ed8d44f0dd86ca5d881dc3ce2::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit78b2fc3ed8d44f0dd86ca5d881dc3ce2::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInit78b2fc3ed8d44f0dd86ca5d881dc3ce2::$classMap;
 
         }, null, ClassLoader::class);
     }
