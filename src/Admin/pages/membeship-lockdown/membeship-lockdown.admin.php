3b1a89a59ac905c904ceaeeb775271a9801ff9df
devuri <devuriko@gmail.com>
2021-01-17 05:02:43 -0500

    feat: setting to Disable the REST API

--- src/Admin/pages/membeship-lockdown/membeship-lockdown.admin.php
+++ src/Admin/pages/membeship-lockdown/membeship-lockdown.admin.php
@@ -11,7 +11,17 @@ if ( isset( $_POST['submit'] ) ) { // @codingStandardsIgnoreLine
 		wp_die( $this->form()->user_feedback( 'error', 'Verification Failed !!!' ) ); // @codingStandardsIgnoreLine
 	}
 
-	// get the lcg_value.
+	// get rest api val.
+	$disable_rest_api = $this->form()->input_val( 'disable_rest_api' );
+	if ( 'on' === $disable_rest_api ) {
+		$disable_rest_api = 1;
+	} else {
+		$disable_rest_api = 0;
+	}
+	$disable_rest_api = absint( $disable_rest_api );
+	//update_option( 'mlockdown_rest_api', $disable_rest_api );
+
+	// get lockdown val
 	$slockdown = $this->form()->input_val( 'membership_lockdown' );
 
 	// clean up before we save.
@@ -39,12 +49,22 @@ if ( isset( $_POST['submit'] ) ) { // @codingStandardsIgnoreLine
 		<br/>
 		* All data will be locked behind authentication
 		<br/>
-		* Including REST API data will not be available.
+		* You can choose to Disable the REST API.
+		<br/>
+		* If not checked the REST API data will be available.
 </div>
 <hr/>
 <div id="frmwrap" >
 	<form action="" method="POST"	enctype="multipart/form-data">
 		<?php
+			// checkbox.
+			$color    = '#424242';
+			$checkbox = '<div id="rest_api_checkbox" style="color:' . $color . ';font-weight:600">';
+			$checkbox .= '<input type="checkbox" id="disable_rest_api" name="disable_rest_api" style="margin:unset;">';
+			$checkbox .= '<label for="disable_rest_api" style="padding-left: 0.5em;"> Disable REST API</label>';
+			$checkbox .= '</div>';
+			echo $checkbox;
+
 			// submit button.
 			echo LockItdown::setup()->lock_button(); // @codingStandardsIgnoreLine
 
@@ -53,3 +73,16 @@ if ( isset( $_POST['submit'] ) ) { // @codingStandardsIgnoreLine
 		?>
 	</form>
 </div><!--frmwrap-->
+<script type="text/javascript">
+	jQuery( document ).ready( function( $ ) {
+
+		// selection
+		jQuery('input[type="checkbox"]').on('click', function( event ){
+			$(this).parent().css('background-color', '#fff').css('color', '#424242');
+				if ($(this).is(":checked")) {
+					$(this).parent().css('background-color', '#fff').css('color', '#b9b9b9');
+			}
+		});
+
+	});
+</script>
